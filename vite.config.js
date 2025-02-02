5f3d4a0f7ba087e928c065ea72bd77e4fbb2d6cc
Ganesh Ganjave <ganuganjave@gmail.com>
2025-02-03 03:09:26 +0530

    Second commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
-    tailwindcss(),
   ],
   assetsInclude: ['**/*.MP4', '**/*.MOV'], // Combined both file types into a single array
   base: "/video-edits",
